c0898584c97a83055be85c743399560d0fc59563
Duvan Diaz <47454957+KillTheReptiles@users.noreply.github.com>
2023-01-19 15:46:37 -0500

    portafolio update

---
+++ js/template-footer.js
@@ -0,0 +1,36 @@
+document.write(`
+
+<footer class="footer py-4">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4 text-lg-start">Copyright &copy; Duvan Diaz 2023</div>
+                <div class="col-lg-4 my-3 my-lg-0">
+                    <a class="btn btn-dark btn-social mx-2" href="https://github.com/KillTheReptiles" target="_blank" aria-label="GitHub"><i
+                        class="fab fa-github"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="https://twitter.com/kt17_official" target="_blank" aria-label="Twitter"><i
+                            class="fab fa-twitter"></i></a>
+                    <a class="btn btn-dark btn-social mx-2" href="https://www.linkedin.com/in/duvandiazmz/" target="_blank" aria-label="LinkedIn"><i
+                            class="fab fa-linkedin-in"></i></a>
+                </div>
+                <div class="col-lg-4 text-lg-end">
+                    <!--<a class="link-dark text-decoration-none me-3" href="#!">Privacy Policy</a>
+                    <a class="link-dark text-decoration-none" href="#!">Terms of Use</a>-->
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LCFX2CCP6C"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LCFX2CCP6C');
+</script>
+`);
---
+++ js/template-header.js
@@ -0,0 +1,26 @@
+document.write(`
+<script src= "../js/scripts.js"></script>
+
+ <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="duvandiaz.com/#page-top">
+                <!--<img src="assets/img/.svg" alt="..." />!-->Duvan Diaz
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars ms-1"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="duvandiaz.com/#page-top">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="duvandiaz.com/#portfolio">Portfolio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="duvandiaz.com/#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="duvandiaz.com/#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+
+`);
